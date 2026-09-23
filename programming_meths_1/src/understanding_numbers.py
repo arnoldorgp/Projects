bc4ee640d572b65211e839293b5a766eaab8b701
Arnoldo <2630202@upv.edu.mx>
2026-09-23 09:02:19 -0600

    vimos errores de sintaxis y una pequeña introduccion a los numeros en las variables 23/09/2026

---
+++ programming_meths_1/src/understanding_numbers.py
@@ -0,0 +1,15 @@
+# Números
+# Enteros - Integers
+"""
+ Los números enteros los podemos
+ sumar (+), restar (-), multiplicar(*)
+ y dividir (/)
+"""
+
+print(2+3)
+print(3-2)
+print(2*3)
+print(3/2)
+number_1=5
+number_2=10
+print(number_1+number_2)
---
+++ programming_meths_1/src/understanding_sintax_error.py
@@ -0,0 +1,10 @@
+#Eliminación de espacios en blanco
+programming_language = "    python    "
+print(programming_language)
+print("Efecto del lstrip")
+print(programming_language.lstrip())
+print("Efecto del rstrip")
+print(programming_language.rstrip())
+print("Efecto del strip")
+print(programming_language.strip())
+print(programming_language)
